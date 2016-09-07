1e2b45ce5f28a83982b4230fbf088dcbd25a5819
Marco <bartel@electronic-shop.lu>
2016-09-07 08:59:08 +0200

    addes setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='xyaml',
     version='0.9.1',
-    packages=['PyYAML'],
+    packages=[''],
     url='https://github.com/bsimpson888/xyaml',
     license='GPL',
     author='Marco Bartel',
